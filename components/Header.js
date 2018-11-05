5d7eec20873593dfafc209d8107023f8d43c56b3
Huong Dang <huongd97@knights.ucf.edu>
2018-11-05 00:32:37 -0500

    Add head info to html page

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 const linkStyle = {
     marginRight: 15
@@ -6,6 +7,9 @@ const linkStyle = {
 
 const Header = () => (
     <div>
+        <Head>
+            <title>College Event Web Application</title>
+        </Head>
         <Link href="/">
             <a style={linkStyle}>Home</a>
         </Link>
