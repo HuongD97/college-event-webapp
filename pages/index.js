f00badd3043a244e8196c3d81152c199301d349b
Huong Dang <huongd97@knights.ucf.edu>
2018-11-05 01:00:34 -0500

    Play with getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
 
 const buttonStyle = {
     fontSize: '20px',
@@ -7,26 +8,32 @@ const buttonStyle = {
     borderRadius: '10px',
     padding: 10,
     color: '#e9dbf9',
-    backgroundColor: '#3e1472'
+    backgroundColor: '#3e1472',
 };
 
-const PostLink = (props) => (
-    <li>
-        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-);
-
-export default () => {
+const Index = props => {
     return (
         <Layout>
-            <p>Hello Next.JS!</p>
+            <h1>Batman TV Shows</h1>
             <ul>
-                <PostLink title={'Hello Next.js'} id={'hello-nextjs'}/>
-                <PostLink title={'Learn Next.js is awesome'} id={'learn-nextjs'}/>
-                <PostLink title={'Deploy apps with Zeit'} id={'deploy-nextjs'}/>
+                {props.shows.map(({ show }) => (
+                    <li key={show.id}>
+                        <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                            <a>{show.name}</a>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </Layout>
     );
 };
+
+Index.getInitialProps = async (context) => {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+    return {
+        shows: data,
+    };
+};
+
+export default Index;
