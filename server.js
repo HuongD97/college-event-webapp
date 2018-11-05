cf86eb000b35a53d67df75f7c92b5f4a12b9297d
Huong Dang <huongd97@knights.ucf.edu>
2018-11-05 00:29:17 -0500

    Map masked route to actual route for server side rendering

--- server.js
+++ server.js
@@ -12,16 +12,26 @@ app.prepare()
     .then(() => {
         const server = express();
 
+        server.get('/p/:id', (req, res) => {
+            const actualPage = '/post';
+            const queryParams = { title: req.params.id };
+
+            // Not sure what this line is doing. But pretty
+            // sure it's routing the incoming request to the masked
+            // route to the actual route
+            app.render(req, res, actualPage, queryParams);
+        });
+
         server.get('*', (req, res) => {
             return handle(req, res);
         });
 
-        server.listen(3000, (err) => {
+        server.listen(3000, err => {
             if (err) throw err;
             console.log('> Read on http://localhost:3000');
         });
     })
-    .catch((ex) => {
+    .catch(ex => {
         console.error(ex.stack);
         process.exit(1);
     });
