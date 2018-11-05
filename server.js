7ca60da71078bbb7f36b78d2d9647d30ec725de8
Huong Dang <huongd97@knights.ucf.edu>
2018-11-05 00:03:16 -0500

    Add server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const next = require('next');
+
+// Check if we're in the dev environment or production
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const PORT = process.env.PORT || 3000;
+
+const handle = app.getRequestHandler();
+
+app.prepare()
+    .then(() => {
+        const server = express();
+
+        server.get('*', (req, res) => {
+            return handle(req, res);
+        });
+
+        server.listen(3000, (err) => {
+            if (err) throw err;
+            console.log('> Read on http://localhost:3000');
+        });
+    })
+    .catch((ex) => {
+        console.error(ex.stack);
+        process.exit(1);
+    });
