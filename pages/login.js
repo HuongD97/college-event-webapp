1967c0db4bd48d3182bec060a7355833197a6da4
Huong Dang <huongd97@knights.ucf.edu>
2018-11-06 23:31:14 -0500

    account: Update .gitignore to ignore file containing environment variables and update login page to login a user

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import fire from '../fire';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userID: null,
+        };
+    }
+
+    componentDidMount() {
+        fire.auth()
+            .signInWithEmailAndPassword('d', '12345678')
+            .then(() => {
+                const user = fire.auth().currentUser;
+                this.setState({ userID: user ? user.uid : null });
+            })
+            .catch(error => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(error);
+            });
+    }
+
     render() {
-        return (
-            <div>
-                Login here!
-            </div>
-        );
-    };
+        if (this.state.userID) {
+            return (
+                <div>{`user ${this.state.userID} is currently logged in!`}</div>
+            );
+        } else {
+            return <div>{`No user is currently logged in.`}</div>;
+        }
+    }
 }
 
 export default Login;
