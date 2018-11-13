e85b2a41cc3778951990d831d756314aff1cf342
Huong Dang <huongd97@knights.ucf.edu>
2018-11-12 23:33:30 -0500

    Add exported functions to create, getUser and getAll users

--- model/Users.js
+++ model/Users.js
@@ -1,10 +1,43 @@
 const db = require('../db');
 
-exports.getAll = (done) => {
-    console.log('aaaaaaah');
-    db.get().query('SELECT * FROM Users', (err, result) => {
-        if (err) throw err;
-        console.log('currently in exports.getAll', JSON.stringify(result));
-        done(null, result);
-    });
+exports.getAll = callback => {
+    const query = `SELECT * FROM Users`;
+    const retVal = { err: null, result: [] };
+
+    db.get()
+        .query(query)
+        .on('error', err => (retVal.error = err))
+        .on('result', row => {
+            retVal.result.push(row);
+        })
+        .on('end', () => callback(retVal));
+};
+
+exports.getUser = (uid, callback) => {
+    const query = `SELECT * FROM Users where uid = '${uid}'`;
+    const retVal = [];
+    db.get()
+        .query(query)
+        .on('error', err => {
+            console.error(err);
+        })
+        .on('result', row => {
+            retVal.push(row);
+        })
+        .on('end', () => {
+            callback({ user: retVal[0] || {} });
+        });
+};
+
+exports.create = (firstName, lastName, email, uid, callback) => {
+    const retVal = {};
+    const query = `INSERT INTO Users (firstName, lastName, email, uid) VALUES ('${firstName}', '${lastName}', '${email}', '${uid}')`;
+
+    db.get()
+        .query(query)
+        .on('error', err => (retVal.error = err))
+        .on('result', result => {
+            retVal.success = true;
+        })
+        .on('end', () => callback(retVal));
 };
