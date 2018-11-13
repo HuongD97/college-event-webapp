32c940402730c08c3b42f66eecbeb42116e21279
Huong Dang <huongd97@knights.ucf.edu>
2018-11-12 23:33:52 -0500

    Clean up server.js by extracting out the database calls to the model

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const next = require('next');
-const db = require('./db');
 // Check if we're in the dev environment or production
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const PORT = process.env.PORT || 3000;
-const getAll = require('./model/Users');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
-const connection = mysql.createConnection({
+const Users = require('./model/Users');
+const database = require('./db');
+
+const db = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
     password: process.env.password,
@@ -21,64 +24,54 @@ app.prepare()
     .then(() => {
         // Connect to the database
         const server = express();
-        server.get('/api/whoot', (req, res) => {
-            res.json({ text: 'hello1' });
+
+        // Make a database connection
+        database.connect();
+
+        server.post('/create', jsonParser, (req, res) => {
+            const { firstName, lastName, email, uid } = req.body;
+            if (!firstName || !lastName || !email || !uid) {
+                return res.status(500).json({
+                    error: `First name, last name, email, or uid is missing`,
+                });
+            }
+
+            Users.create(firstName, lastName, email, uid, result =>
+                res.json(result),
+            );
+
+            //
+            // const retVal = {};
+            // const query = `INSERT INTO Users (firstName, lastName, email, uid) VALUES ('${firstName}', '${lastName}', '${email}', '${uid}')`;
+
+            // return db
+            //     .query(query)
+            //     .on('error', err => (retVal.error = err))
+            //     .on('result', result => {
+            //         console.log(result);
+            //         retVal.success = true;
+            //     })
+            //     .on('end', () => res.json(retVal));
         });
 
-        server.get('/getAll', (req, res) => {
-            try {
-                connection.connect(err => {
-                    console.log('Connected!');
-                    connection.query('SELECT * FROM Users', (err, result) => {
-                        if (err) throw err;
-
-                        console.log(JSON.stringify(result));
-                        res.send(result);
-                    });
-                });
-            } catch (e) {
-                console.error(JSON.stringify(e));
-            }
-        });
-
-        server.get('/create', (req, res) => {
-            try {
-                console.log('hitting post endpoint!');
-                connection.connect(err => {
-                    console.log('Connected!');
-                    connection.query(
-                        `INSERT INTO Users (firstName, lastName, email, uid) VALUES ('${
-                            req.params.firstName
-                        }', '${req.params.lastName}', '${
-                            req.params.email
-                        }', '${req.params.uid}')`,
-                        (err, result) => {
-                            console.log(JSON.stringify(result));
-                            res.json({ success: true });
-                        },
-                    );
-                });
-            } catch (e) {
-                console.error(JSON.stringify(e));
-            }
+        server.get('/allUsers', (req, res) => {
+            Users.getAll(result => res.json(result));
         });
 
         server.get('/user', (req, res) => {
-            try {
-                const dest = '/loggedIn';
-                connection.connect(err => {
-                    console.log('Connected!');
-                    connection.query(
-                        `SELECT * FROM Users WHERE uid = '${req.query.uid}'`,
-                        (err, result) => {
-                            console.log(JSON.stringify(result));
-                            app.render(req, res, dest, { user: result[0] });
-                        },
-                    );
+            const dest = '/loggedIn';
+            const failDest = '/';
+            const retVal = { user: null, error: null };
+            const { uid } = req.query;
+
+            if (!uid) {
+                return res.status(500).json({
+                    error: `No uid provided!`,
                 });
-            } catch (e) {
-                console.error(JSON.stringify(e));
             }
+            Users.getUser(uid, result => {
+                app.render(req, res, dest, { user: result.user });
+            });
         });
 
         server.get('*', (req, res) => {
@@ -89,15 +82,6 @@ app.prepare()
             if (err) throw err;
             console.log('> Read on http://localhost:3000');
         });
-
-        // db.connect()
-        //     .then(() => {
-        //         server.listen(3000, err => {
-        //             if (err) throw err;
-        //             console.log('> Read on http://localhost:3000');
-        //         });
-        //     })
-        //     .catch(err => console.error(JSON.stringify(err)));
     })
     .catch(ex => {
         console.error(ex.stack);
