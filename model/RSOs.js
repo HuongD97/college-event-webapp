6692ef340d7af03f19874d40d7eb1cfa2c480b82
Huong Dang <huongd97@knights.ucf.edu>
2018-11-15 01:09:01 -0500

    Query the database to find all RSOs existing the university

--- model/RSOs.js
+++ model/RSOs.js
@@ -13,3 +13,20 @@ exports.getAllStudentsFromUniversity = (university, callback) => {
         });
     }
 };
+
+exports.getAllRSOsFromUni = (university, callback) => {
+    if (!university)
+        callback({ errorMessage: `Must provide a university name!` });
+    else {
+        const query = `select * from RSOs r where r.university = '${university}'`;
+
+        db.get().query(query, (err, results) => {
+            if (err) callback(err);
+            else {
+                callback(null, results);
+            }
+        });
+    }
+};
+
+exports.createRSO = (rsoInfo, callback) => {};
