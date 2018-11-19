5b2000fed1767c02a7f4ca2a5089626a7c748825
Huong Dang <huongd97@knights.ucf.edu>
2018-11-19 03:08:16 -0500

    Adding Events model to interact with database

---
+++ model/Events.js
@@ -0,0 +1,27 @@
+const db = require('../db');
+const async = require('async');
+const Users = require('./Users');
+const RSOs = require('./RSOs');
+
+// const {
+//   event_name,
+//   event_location,
+//   event_time,
+//   event_description,
+//   event_id,
+//   admin_id,
+//   admin_email,
+//   event_comment,
+// } = props.eventInfo;
+exports.getAllPublicEvents = callback => {
+    const query = `select * 
+    from Events e, Public_events p, Locations l, Admins a, Users u
+    where e.event_id=p.public_event_id and l.location_id=e.event_location and p.admin_id = a.admin_id and a.admin_id = u.uid;`;
+
+    db.get().query(query, (err, results) => {
+        if (err) callback(err);
+        else {
+            callback(null, results);
+        }
+    });
+};
