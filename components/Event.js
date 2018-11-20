2e7d6a27c21e0d4ce442b9a5cdac7dffc4954b82
Huong Dang <huongd97@knights.ucf.edu>
2018-11-19 22:26:04 -0500

    Pull comment section to a functional comment

--- components/Event.js
+++ components/Event.js
@@ -1,11 +1,204 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import {
+    Card,
+    CardContent,
+    Typography,
+    Button,
+    CircularProgress,
+    TextField,
+    Select,
+    InputLabel,
+    MenuItem,
+    FormControl,
+} from '@material-ui/core';
 import Break from './Break';
 import moment from 'moment';
+import axios from 'axios/index';
+// import classNames from 'classnames';
 
 const formatDateAndTime = timestamp => {
     return moment(timestamp).format('MMMM Do YYYY, h:mm:ss a');
+};
+const styles = {
+    textField: {
+        flexBasis: 100,
+    },
+};
+const Comment = props => {
+    const { event_id } = props.eventInfo;
+    const { uid } = props.user;
+
+    const [loading, setLoading] = useState(false);
+    const [commentAndRating, setCommentAndRating] = useState({
+        comment: '',
+        rating: -1,
+    });
+    const [comment, setComment] = useState('');
+    const [rating, setRating] = useState('');
+    const [editing, setEditing] = useState(false);
+    const [saving, setSaving] = useState(false);
+
+    // Asynchronous function to update user's comment
+    const getUserCommentAndRatingForEvent = async () => {
+        try {
+            const res = await axios.post('/userComment', {
+                uid,
+                event_id,
+            });
+
+            return res.data.userComment[0];
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    // Fetch the user's comment and update state
+    useEffect(() => {
+        getUserCommentAndRatingForEvent().then(userFeedback => {
+            const { content, rating } = userFeedback || {
+                content: '',
+                rating: -1,
+            };
+            setCommentAndRating({ comment: content, rating: rating });
+            setComment(content);
+            setRating(rating);
+        });
+    }, []);
+
+    const Edit = () => {
+        return (
+            <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => setEditing(true)}
+            >
+                Edit
+            </Button>
+        );
+    };
+
+    const Save = () => {
+        return (
+            <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => console.log('saving')}
+            >
+                Save
+            </Button>
+        );
+    };
+
+    const Delete = () => {
+        return (
+            <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => console.log('deleting')}
+                disabled={saving}
+            >
+                Delete
+            </Button>
+        );
+    };
+
+    const Cancel = () => {
+        return (
+            <Button
+                variant="outlined"
+                onClick={() => setEditing(false)}
+                disabled={saving}
+            >
+                Cancel
+            </Button>
+        );
+    };
+
+    const getRating = rating => {
+        return rating > -1 ? `${rating} / 5` : 'Not Rated';
+    };
+
+    const getComment = comment => {
+        return comment.length > 0 ? `${comment}` : 'No comment yet';
+    };
+
+    const renderEditView = () => {
+        const handleChange = name => event => {
+            switch (name) {
+                case 'comment':
+                    setComment(event.target.value);
+                    break;
+                case 'rating':
+                    setRating(event.target.value);
+                    break;
+            }
+        };
+        return (
+            <div
+                style={{
+                    borderStyle: 'solid',
+                    borderWidth: '0.5px',
+                    padding: '20px',
+                    borderRadius: '20px',
+                }}
+            >
+                <FormControl>
+                    <InputLabel htmlFor="rating">Rating</InputLabel>
+                    <Select value={rating} onChange={handleChange('rating')}>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                        <MenuItem value={5}>5</MenuItem>
+                    </Select>
+                </FormControl>
+                <Break height={15} />
+                <TextField
+                    label="Comment"
+                    type="text"
+                    name="comment"
+                    value={comment}
+                    onChange={handleChange('comment')}
+                    multiline={true}
+                    rowsMax={4}
+                    fullWidth
+                />
+                <Break height={15} />
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <Save />
+                    <div style={{ marginRight: '5px' }} />
+                    <Delete />
+                    <div style={{ marginRight: '5px' }} />
+                    <Cancel />
+                </div>
+            </div>
+        );
+    };
+
+    const renderView = () => {
+        return (
+            <div
+                style={{
+                    borderStyle: 'solid',
+                    borderWidth: '0.5px',
+                    padding: '20px',
+                    borderRadius: '20px',
+                }}
+            >
+                <Typography component="p">
+                    Rating: <b>{getRating(commentAndRating.rating)}</b>
+                </Typography>
+                <Typography component="p">
+                    Comment: <b>{getComment(commentAndRating.comment)}</b>
+                </Typography>
+                <Break height={15} />
+                <Edit />
+            </div>
+        );
+    };
+
+    return editing ? renderEditView() : renderView();
 };
 
 const Event = props => {
@@ -13,7 +206,6 @@ const Event = props => {
         event_name,
         event_time,
         event_description,
-        event_comment,
         location_name,
         university,
         firstName,
@@ -47,9 +239,8 @@ const Event = props => {
                 <Typography component="p">
                     Contact email: <b>{`${email}`}</b>
                 </Typography>
-                <Typography component="p">
-                    Comment: <b>{event_comment}</b>
-                </Typography>
+                <Break height={15} />
+                <Comment user={props.user} eventInfo={props.eventInfo} />
                 <Break height={15} />
             </CardContent>
         </Card>
